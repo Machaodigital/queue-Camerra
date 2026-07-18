0c1f3c0344cb62f2ee6ef19b236a71d94302bf52
Machaodigital <ynatcha550@gmail.com>
2026-07-19 02:42:25 +0700

    จัดการ์ดกล้องเป็น 2 คอลัมน์

--- camera-queue/src/data/cameraData.js
+++ camera-queue/src/data/cameraData.js
@@ -17,14 +17,22 @@ const cameras = [
 
   {
     id:3,
-    name:"Canon IXY 31S",
-    image:"/images/ixy31s.png",
+    name:"Canon IXY 31S Gold",
+    image:"/images/ixy31sGold.png",
     booked:[],
     className:"ixy31s"
   },
 
   {
     id:4,
+    name:"Canon IXY 31S Pink",
+    image:"/images/ixy31s.png",
+    booked:[],
+    className:"ixy31s"
+  },
+
+  {
+    id:5,
     name:"Canon IXY 510IS",
     image:"/images/ixy510is.png",
     booked:[],
@@ -32,7 +40,7 @@ const cameras = [
   },
 
   {
-    id:5,
+    id:6,
     name:"Canon IXY 930IS",
     image:"/images/ixy930is.png",
     booked:[],
@@ -40,20 +48,22 @@ const cameras = [
   },
 
   {
-    id:6,
+    id:7,
     name:"Canon IXY 920IS",
-    image:"/images/ixy920is.jpg",
+    image:"/images/ixy920is.png",
     booked:[],
     className:"ixy920is"
   },
 
   {
-    id:7,
+    id:8,
     name:"Canon Powershot A800",
-    image:"/images/a800.jpg",
+    image:"/images/A800.png",
     booked:[],
     className:"a800"
   },
+
+  
 ]
 
 
